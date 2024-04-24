81da26c26afc7226cf6496da5fed53c32ee0c493
Hong-JunHyeok <edb1631@naver.com>
2024-04-25 00:38:02 +0900

    hlog favicon update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,24 @@
 import { Suspense } from "react";
 
+import * as shared from "@/shared";
 import { ArticleList } from "@/entities/article";
+
+const Skeleton = () => {
+  return (
+    <>
+      <shared.Skeleton height={15} />
+      <shared.Skeleton height={15} />
+      <shared.Skeleton height={15} />
+      <shared.Skeleton height={15} />
+      <shared.Skeleton height={15} />
+    </>
+  );
+};
 
 function HomePage() {
   return (
     <div className="w-full">
-      <Suspense fallback={<>Loading...</>}>
+      <Suspense fallback={<Skeleton />}>
         <ArticleList />
       </Suspense>
     </div>

0c77ee9adc95a262320e5ac03618a91fd26d114d
Hong-JunHyeok <edb1631@naver.com>
2024-05-17 04:42:04 +0900

    Header User Division update

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, If, useSession } from "@/shared";
+import { Skeleton, If, useSession, useFetchUser } from "@/shared";
 import { Suspense } from "react";
 import { LuSearch } from "react-icons/lu";
 import { FaPen } from "react-icons/fa";
@@ -7,30 +7,25 @@ import { useToastStore } from "@/app/store";
 
 const UserDivision = () => {
   const { data } = useSession();
+  const { data: userData } = useFetchUser(data.session?.user.id ?? "");
   const navigate = useNavigate();
 
   const BUTTON_CLASSNAME =
     "text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center";
-
-  if (!data) return null;
 
   return (
     <li>
       <If
         condition={!!data.session}
         trueRender={
-          <button
-            type="button"
-            className={BUTTON_CLASSNAME}
+          <img
+            src={userData?.profile_url}
+            className="w-8 h-8 rounded-full shadow-sm cursor-pointer"
+            alt={userData?.username}
             onClick={() => {
               navigate(`/profile/${data.session?.user.id}`);
             }}
-          >
-            {/* NOTE: Github Provider는 user_name 형식, Email Provider는 username 형식이기 때문 */}
-            {(data.session?.user.user_metadata.user_name ||
-              data.session?.user.user_metadata.username) ??
-              "닉네임이 설정되지 않았습니다 (개발자에게 문의해주세요)"}
-          </button>
+          />
         }
         falseRender={
           <button
@@ -80,7 +75,7 @@ function Header() {
               });
             }}
           >
-            <LuSearch size={30} />
+            <LuSearch size={24} />
           </div>
         </li>
 
@@ -89,7 +84,7 @@ function Header() {
             className="mr-8 cursor-pointer"
             onClick={() => navigate("/article-write")}
           >
-            <FaPen size={30} />
+            <FaPen size={24} />
           </div>
         </li>
 

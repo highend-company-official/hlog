634cbc0c2b303f04e972dcbc9db05976e914140f
Hong-JunHyeok <edb1631@naver.com>
2024-06-17 13:22:13 +0900

    자잘한 스타일 수정

--- src/widgets/editor/ui/custom-block/code-block.tsx
+++ src/widgets/editor/ui/custom-block/code-block.tsx
@@ -1,6 +1,6 @@
 const CodeBlock = (props: any) => {
   return (
-    <pre className="p-4 text-white transition ease-in-out bg-black shadow-md rounded-xl">
+    <pre className="p-4 text-white transition ease-in-out bg-black shadow-md leading-8 rounded-xl">
       {props.children}
     </pre>
   );

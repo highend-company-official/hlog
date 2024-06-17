49416dfba3c8e9e6bcce17854e540070ca112753
Hong-JunHyeok <edb1631@naver.com>
2024-06-17 19:25:34 +0900

    sitemap router

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import * as path from "path";
 import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,7 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    Pages({
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+  ],
   base: "/",
   define: {
     global: "window",

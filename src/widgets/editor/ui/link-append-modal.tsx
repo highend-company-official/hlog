8100bb6c61ebcae1017ed9723a52f22179870037
Hong-JunHyeok <edb1631@naver.com>
2024-06-17 14:27:15 +0900

    WIP

--- src/widgets/editor/ui/link-append-modal.tsx
+++ src/widgets/editor/ui/link-append-modal.tsx
@@ -1,16 +1,16 @@
-import { Modal } from "@/shared";
+// import { Modal } from "@/shared";
 
-type Props = {
-  onClose: () => void;
-  open: boolean;
-};
+// type Props = {
+//   onClose: () => void;
+//   open: boolean;
+// };
 
-const LinkAppendModal = ({ onClose, open }: Props) => {
-  return (
-    <Modal open={open}>
-      <Modal.Header>URL을 입력해주세요.</Modal.Header>
-    </Modal>
-  );
-};
+// const LinkAppendModal = ({ onClose, open }: Props) => {
+//   return (
+//     <Modal open={open}>
+//       <Modal.Header>URL을 입력해주세요.</Modal.Header>
+//     </Modal>
+//   );
+// };
 
-export default LinkAppendModal;
+// export default LinkAppendModal;

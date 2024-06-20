487e84dcabee9a440d2416cffefc99ae2d5369d8
Hong-JunHyeok <edb1631@naver.com>
2024-06-20 13:57:16 +0900

    광고 에러 수정

--- src/shared/components/ads-block.tsx
+++ src/shared/components/ads-block.tsx
@@ -26,7 +26,7 @@ const AdsBlock = ({ className, client, slot, format, responsive }: Props) => {
 
   return (
     <ins
-      className={className}
+      className={"adsbygoogle" + className}
       data-ad-client={client}
       data-ad-slot={slot}
       data-ad-format={format}
